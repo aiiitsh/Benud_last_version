f6e204d9d6edb4f86ea4c0a857fd877f3b149c35
aiiitsh <ahesham368@gmail.com>
2023-10-29 21:58:36 +0200

    last updates

--- navigators/screens/Customers.js
+++ navigators/screens/Customers.js
@@ -1,25 +1,30 @@
-//Help page
-import { View, Text } from 'react-native'
+
+import { Linking } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import KeyboardAvoidingWrapper from '../../components/KeyboardAvoidingWrapper'
 import { StyledContainer } from '../../components/styles';
 import { InnerContainer } from '../../components/styles';
 import { PageTitle } from '../../components/styles';
 import { SubTitle } from '../../components/styles';
+
 export default function Customers() {
+  const email = 'benudhelp@gmail.com';
+
+  const handleEmailPress = () => {
+    Linking.openURL(`mailto:${email}`);
+  }
+
   return (
     <KeyboardAvoidingWrapper>
       <StyledContainer>
-        
         <InnerContainer>
-          
           <PageTitle>للتواصل</PageTitle>
-          <SubTitle>benudhelp@gmail.com</SubTitle>
-          
+          <TouchableOpacity onPress={handleEmailPress}>
+            <SubTitle>{email}</SubTitle>
+          </TouchableOpacity>
         </InnerContainer>
       </StyledContainer>
     </KeyboardAvoidingWrapper>
   );
 }
-
-//صفحة الاعدادات
